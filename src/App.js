af4b19131f75c77298869f91c83e0f034a9fdd4d
wangchen <2090861117@qq.com>
2020-02-25 23:13:10 +0800

    掌握grid布局

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import c from 'classnames'
+import css from './App.module.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={css.container}>
+      <div className={css.title}>
+        <span className={css.titleContent}>Grid 布局</span>
+      </div>
+      <div className={css.gridContainer}>
+        <div
+          className={c(css.gridItem, css.grid1)}
+          style={{ backgroundColor: '#009922' }}
         >
-          Learn React
-        </a>
-      </header>
+          1
+        </div>
+        <div
+          className={c(css.gridItem, css.grid2)}
+          style={{ backgroundColor: '#00FF22' }}
+        >
+          2
+        </div>
+        <div
+          className={c(css.gridItem, css.grid3)}
+          style={{ backgroundColor: '#FF1122' }}
+        >
+          3
+        </div>
+        <div
+          className={c(css.gridItem, css.grid4)}
+          style={{ backgroundColor: '#991199' }}
+        >
+          4
+        </div>
+        <div
+          className={c(css.gridItem, css.grid5)}
+          style={{ backgroundColor: '#0011FF' }}
+        >
+          5
+        </div>
+        <div
+          className={c(css.gridItem, css.grid6)}
+          style={{ backgroundColor: '#FF11FF' }}
+        >
+          6
+        </div>
+        <div
+          className={c(css.gridItem, css.grid7)}
+          style={{ backgroundColor: '#0000FF' }}
+        >
+          7
+        </div>
+        <div
+          className={c(css.gridItem, css.grid8)}
+          style={{ backgroundColor: '#AA11FF' }}
+        >
+          8
+        </div>
+        <div
+          className={c(css.gridItem, css.grid9)}
+          style={{ backgroundColor: '#33FF99' }}
+        >
+          9
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
